7db0193d07827783aaa292f0cbb0ce42ab804089
jemappellecasey <casey.lee.black@gmail.com>
2018-07-25 11:12:35 -0600

    added dark skew

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,11 @@ export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div>hi</div>
-        <div>hi</div>
-        <div>hi</div>
+      <div className="meh"></div>
         <div className="grid__skew-light-one"></div>
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
+        <div className="grid__skew-dark"></div>
       </div>
     );
   }
