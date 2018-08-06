52a8658fc298497c66b4bbc13b08f52babdbcf3d
jemappellecasey <casey.lee.black@gmail.com>
2018-08-06 12:57:42 -0500

    fixed duplicate interval by clearing when you generate

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   constructor(props) {
     super(props)
 
-    var timer = 0;
+    this.timer = 0;
 
     this.state = {
       active: false,
@@ -25,65 +25,64 @@ export default class App extends Component {
     }
   }
 
-  handleChange= function(date) {
-    console.log("APP JS WORKING")
+  handleChange = function (date) {
+    clearInterval(this.timer);
     this.setState({
       startDate: date
     });
   }.bind(this)
 
-  handleGenerate = function() {
-    this.setState({active:true})
+  handleGenerate = function () {
+    this.setState({ active: true })
     // set the date we're counting down to
     var countDownDate = this.state.startDate.toDate().getTime();
 
     // Update the count every 1 second
-    timer = setInterval(function() {
+    this.timer = setInterval(function () {
 
       // Get today's date and time
       var now = new Date().getTime();
-    
+
       // Find distance between the Countdown date and now
       var distance = countDownDate - now;
-    
+
       // Time Calculations
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
       // Output the results in an element with id="demo"
       const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-      
-      
-        const timeRemaining = {
-          days,
-          hours,
-          minutes,
-          seconds
-        }
-      this.setState({ active:true })
-      console.log(this.state.timeRemaining);
+
+
+      const timeRemaining = {
+        days,
+        hours,
+        minutes,
+        seconds
+      }
+      this.setState({ timeRemaining })
 
       if (distance < 0) {
-        clearInterval(x);
+        clearInterval(this.timer);
 
       }
 
-    }, 1000);
+    }.bind(this), 1000);
   }.bind(this)
-  
-  renderItems = function() {
-    if(this.state.active) {
+
+  renderItems = function () {
+    if (this.state.active) {
       return [
-        <Clock />,
+        <Clock timeRemaining={this.state.timeRemaining}/>,
         ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText('08/13'),
         <label className="grid__remaining">Remaining Until Your 22nd Birthday</label>
       ]
     } else {
       return [
-        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
+        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)} />,
         Button('Generate Countdown', () => this.handleGenerate())
       ]
     }
@@ -101,8 +100,8 @@ export default class App extends Component {
         <div className="grid__skew-dark"></div>
 
 
-        { this.renderItems() }
-        
+        {this.renderItems()}
+
       </div>
     );
   }
--- src/components/clock.js
+++ src/components/clock.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 
 class Clock extends Component {
     render() {
+        const time = this.props.timeRemaining;
         return (
             <div className='clock'>
                 <div className="clock__days clock__box">
                     <label className="clock__title">Days</label>
-                    <label className="clock__amount">200</label>
+                    <label className="clock__amount">{time.days}</label>
                 </div>
                 <div className="clock__hrs clock__box">
                     <label className="clock__title">Hours</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">{time.hours}</label>
                 </div>
                 <div className="clock__min clock__box">
-                    <label className="clock__title">Minuits</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__title">Minutes</label>
+                    <label className="clock__amount">{time.minutes}</label>
                 </div>
                 <div className="clock__sec clock__box">
                     <label className="clock__title">Seconds</label>
-                    <label className="clock__amount">20</label>
+                    <label className="clock__amount">{time.seconds}</label>
                 </div>
             </div>
           )
